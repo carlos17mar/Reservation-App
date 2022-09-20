774bb8dc0b781a253ce66d77a933e9ce84e49348
Carlos <devcarlosmarin@gmail.com>
2022-09-20 17:59:24 +0200

    Advance a little and create the classes

---
+++ src/test/Reserva.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author carlos
+ */
+public class Reserva {
+
+	private String nombre;
+	private String apellido;
+	private int numTlf;
+	private int numPersonas;
+	enum Cocina{BUFE,CARTA,CITA,NO}
+}
